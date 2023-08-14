6abcc796861f26cc20e66889a6439e4c7d653ba8
abdulh4ni <abdulh4ni@icloud.com>
2023-08-14 17:09:11 +0300

    Added navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 import * as React from "react";
 import {Button} from "@nextui-org/button";
 
-
 export default function Home() {
   return (
-    <main>
-      <Button>Press me</Button>
+    <main className="h-screen flex items-center justify-center">
+      <b className="text-xl">W.I.P</b> 
     </main>
   )
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import * as React from "react";
+import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button} from "@nextui-org/react";
+
+
+import { createContext } from 'react';
+
+
+
+export default function Nav() {
+    return (
+        <Navbar position="static">
+            <NavbarBrand>
+                 <b className="">2L8B3DAU</b>
+            </NavbarBrand>
+      </Navbar>
+    )
+}
